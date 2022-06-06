af5642f3dfe1337a2259f06c9fb87f0e0fefb900
Mike Fulton <mikefultonpersonal@gmail.com>
2022-06-05 22:04:01 -0400

    fix up the makefile

--- tarballs/zotsample-1.0/check.sh
+++ tarballs/zotsample-1.0/check.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 if ! [ ./zotsample ]; then
 	echo "zot sample does not work" >&2
+  exit 1
+else
+  echo "All tests passed" 
+  exit 0
 fi
--- tarballs/zotsample-1.0/install.sh
+++ tarballs/zotsample-1.0/install.sh
@@ -5,10 +5,10 @@ if ! [ -d "${prod}" ]; then
 	echo "Installation dir ${prod} from configure does not exist"
 	if ! mkdir -p "${prod}" ; then
 		echo "Installation dir ${prod} from configure does not exist and can not be created" >&2
+	  exit 1
 	else
 		echo "Directory created"
 	fi
-	exit 1
 fi
 if ! cp zotsample "${prod}" ; then
 	echo "Unable to install zotsample into ${prod}" >&2
