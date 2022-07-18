75bfce0ddd0b591803b4cee215167c00b7707552
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:10 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'zotsampleport'), string(name: 'DESCRIPTION', 'zotsampleport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'zotsampleport'), string(name: 'DESCRIPTION', 'A sample z/OS Open Source project' )]
         }
 }
