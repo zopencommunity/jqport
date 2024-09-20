1f80c4ff8e429ace0eaeb901e868f3946bb2eb9b
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:45 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/jqport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/jqport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/jqport.git'), string(name: 'PORT_DESCRIPTION', value: 'a lightweight and flexible command-line JSON processor.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/jqport.git'), string(name: 'PORT_DESCRIPTION', value: 'a lightweight and flexible command-line JSON processor.' )]
         }
 }
